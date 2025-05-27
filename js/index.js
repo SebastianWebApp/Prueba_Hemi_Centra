977496c663eb4c55afe983c0e52cbed297449ed4
SebastianWebApp <mateoespinosam@hotmail.com>
2025-05-26 19:39:00 -0500

    j

--- js/index.js
+++ js/index.js
@@ -3,20 +3,6 @@ document.getElementById("Btn_Buscar").onclick = async function(e){
 
     try {
 
-        var Solicitud = await fetch(`http://ec2-3-86-209-53.compute-1.amazonaws.com/`, {
-            method: "GET",  // Cambiar a POST
-            headers: {
-                "Content-Type": "application/json"  // Especificamos que los datos están en formato JSON
-            }     
-        });
-    
-           var Respuesta_Servidor = await Solicitud.json();
-
-           console.log(Respuesta_Servidor)
-
-           document.getElementById("Imagen").src = Respuesta_Servidor.Respuesta.Logo;
-           document.getElementById("Descripcion").innerText = Respuesta_Servidor.Respuesta.Descripcion;
-
            
         var Solicitud = await fetch(`http://ec2-3-95-182-107.compute-1.amazonaws.com/`, {
             method: "GET",  // Cambiar a POST
@@ -42,6 +28,21 @@ document.getElementById("Btn_Buscar").onclick = async function(e){
         var Respuesta_Servidor = await Solicitud.json();
 
                    document.getElementById("Mascota").src = Respuesta_Servidor.Respuesta;
+
+
+         var Solicitud = await fetch(`http://ec2-3-86-209-53.compute-1.amazonaws.com/`, {
+            method: "GET",  // Cambiar a POST
+            headers: {
+                "Content-Type": "application/json"  // Especificamos que los datos están en formato JSON
+            }     
+        });
+    
+           var Respuesta_Servidor = await Solicitud.json();
+
+           console.log(Respuesta_Servidor)
+
+           document.getElementById("Imagen").src = Respuesta_Servidor.Respuesta.Logo;
+           document.getElementById("Descripcion").innerText = Respuesta_Servidor.Respuesta.Descripcion;
 
         
     } catch (error) {
